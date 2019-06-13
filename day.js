ea4b06960b3d889437043e3941787ae638297cc3
Tasos Zochios <Tasos.Zochios@gmail.com>
2019-06-13 23:40:23 +0300

    Finish tutorial chapter 7

---
+++ day.js
@@ -0,0 +1,9 @@
+// Returns the day of the week for the given date.
+function dayName(date) {
+  const daysOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday",
+                         "Thursday", "Friday", "Saturday"];
+  return daysOfTheWeek[date.getDay()];
+}
+function greeting(now){
+  return `Hello, world! Happy ${dayName(now)}.`;
+}
